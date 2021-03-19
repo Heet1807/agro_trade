66f01a2c965037d7e7340f6ece56cff512741799
Heet1807 <heetnavadiya7@gmail.com>
2021-03-19 17:28:09 +0530

    new database password set

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect("mongodb+srv://heet:heet@123@node.e1eoz.mongodb.net/trade-api",{
+mongoose.connect("mongodb+srv://heet:heet123@node.e1eoz.mongodb.net/trade-api",{
     useCreateIndex:true,
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -9,6 +9,7 @@ mongoose.connect("mongodb+srv://heet:heet@123@node.e1eoz.mongodb.net/trade-api",
 }).then(() => {
     console.log("Connection Successful");
 }).catch((e) => {
+    console.log(e);
     console.log("No Connection");
 });
 // id & passwrod che ke new create kari dav....new kari de OKa
