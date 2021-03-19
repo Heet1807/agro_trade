1ed392b3216bae8f58c5f28e3728795122566319
Heet1807 <heetnavadiya7@gmail.com>
2021-03-19 17:40:07 +0530

    fix

--- db/conn.js
+++ db/conn.js
@@ -12,4 +12,3 @@ mongoose.connect("mongodb+srv://heet:heet123@node.e1eoz.mongodb.net/trade-api",{
     console.log(e);
     console.log("No Connection");
 });
-// id & passwrod che ke new create kari dav....new kari de OKa
