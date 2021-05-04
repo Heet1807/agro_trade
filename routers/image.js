1723928d6b05a90b11fa7aae0e5916dc2532a8dc
Heet1807 <heetnavadiya7@gmail.com>
2021-05-04 14:33:04 +0530

    user register with image cloudinary

--- routers/image.js
+++ routers/image.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var multer = require('multer');
 var router = express.Router();
+var moment = require('moment-timezone');
 var userSchema = require('../models/user');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb){
-        cb(null, 'uploads/');
+        cb(null, 'uploads/image');
     },
     filename: function(req, file, cb){
         cb(null, Date.now() + file.originalname);
@@ -40,44 +41,35 @@ router.post('/getAlldata', async function(req, res, next) {
     }
 });
 
-
-
-// router.post("/register", upload.single('image'), async function(req,res,next){
-//     const { name , email ,phone, password } = req.body;
-//     var fileinfo = req.file;
-//     try {
-//         var existrecord = await userSchema.find( {phone: phone} );
-//         if(existrecord.length == 1){
-//             res.status(200).json({ IsSuccess: true, Data: [], Message: "User Already Exist" });
-//         }else{
-//             var record = await new userSchema({
-//                 name: name,
-//                 email: email,
-//                 phone: phone,
-//                 password : password,
-//                 image: fileinfo == undefined ? "" : fileinfo.path
-//             });
-//             if(record !== null){
-//                 record.save();
-//                 res.status(200).json({ IsSuccess: true , Data: record , Message: "User Register" });
-//             }
-//         }
-//     } catch (error) {
-//         res.status(500).json({ IsSuccess : false , Message: error.message });
-//     }
-// });
-
 router.post("/register", upload.single('image'), async function(req,res,next){
     const { name , email, username ,phone, password } = req.body;
     var fileinfo = req.file;
+    var cloudPath;
     try {
+
+        const cloudinary = require('cloudinary').v2;
+            cloudinary.config({
+                cloud_name: 'drftccumt',
+                api_key: '376913423697972',
+                api_secret: 'uwYuENVthlO4LLQ8KgRaPlVBdIs'
+            });
+
+            var uniqname = "";
+            uniqname = moment().format('MMMM Do YYYY, h:mm:ss a');
+            var c = await cloudinary.uploader.upload(fileinfo.path, { public_id: `Agri_trade/image/${uniqname}`, tags: `Agri_trade` }, function(err, result) {
+                // console.log("Error : ", err);
+                // console.log("Resilt : ", result);
+                cloudPath = result.url;
+            });
+
+
             var record = await new userSchema({
                 name: name,
                 email: email,
                 phone: phone,
                 username: username,
                 password : password,
-                image: fileinfo == undefined ? "" : fileinfo.path
+                image: fileinfo == undefined ? "" : cloudPath
             });
             if(record !== null){
                 record.save();
